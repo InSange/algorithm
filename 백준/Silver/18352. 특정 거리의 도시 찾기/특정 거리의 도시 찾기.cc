e213dc389504576688b87be966fd1933a388a976
선우 <51250442+InSange@users.noreply.github.com>
2024-05-22 01:35:01 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 320 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,60 +1,135 @@
+//#include <iostream>
+//#include <queue>
+//#include <vector>
+//
+//using namespace std;
+//
+//int N, M, K, X, a, b, minDist;
+//deque<deque<int>> town;
+//queue<int> q;
+//vector<int> dist;
+//vector<int> answer;
+//
+//void Solve()
+//{
+//	cin >> N >> M >> K >> X;
+//	N++;
+//	town.assign(N, deque<int>());
+//	dist.assign(N, -1);
+//
+//	for (int i = 0; i < M; i++)
+//	{
+//		cin >> a >> b;
+//
+//		town[a].push_back(b);
+//	}
+//
+//	minDist = -1;
+//	q.push(X);
+//	dist[X] = 0;
+//
+//	while (!q.empty())
+//	{
+//		int cur = q.front();
+//		q.pop();
+//
+//		for (const int& val : town[cur])
+//		{
+//			if (dist[val] != -1) continue;
+//			q.push(val);
+//			dist[val] = dist[cur] + 1;
+//			minDist = max(minDist, dist[val]);
+//		}
+//	}
+//
+//	for (int i = 0; i < dist.size(); i++)
+//	{
+//		if (dist[i] == K) answer.push_back(i);
+//	}
+//
+//	if (answer.size())
+//	{
+//		for (const int& val : answer)
+//		{
+//			cout << val << "\n";
+//		}
+//	}
+//	else cout << -1;
+//}
+//
+//int main()
+//{
+//	cin.tie(nullptr);
+//	ios::sync_with_stdio(false);
+//
+//	Solve();
+//
+//	return 0;
+//}
+
 #include <iostream>
 #include <queue>
 #include <vector>
 
 using namespace std;
 
-int N, M, K, X, a, b, minDist;
-deque<deque<int>> town;
-queue<int> q;
-vector<int> dist;
-vector<int> answer;
+int N, M, K, X, A, B;
+int cur, dist;
+queue<pair<int, int>> q;
+vector<vector<int>> towns;
+vector<bool> visited;
+priority_queue<int, vector<int>, greater<int>> answer;
 
 void Solve()
 {
 	cin >> N >> M >> K >> X;
+
 	N++;
-	town.assign(N, deque<int>());
-	dist.assign(N, -1);
+	towns.assign(N, vector<int>());
+	visited.assign(N, false);
 
-	for (int i = 0; i < M; i++)
+	while (M--)
 	{
-		cin >> a >> b;
-
-		town[a].push_back(b);
+		cin >> A >> B;
+		towns[A].push_back(B);
 	}
 
-	minDist = -1;
-	q.push(X);
-	dist[X] = 0;
+	q.push({ X, 0 });
+	visited[X] = true;
 
 	while (!q.empty())
 	{
-		int cur = q.front();
+		cur = q.front().first;
+		dist = q.front().second;
+
 		q.pop();
 
-		for (const int& val : town[cur])
+		if (dist == K)
 		{
-			if (dist[val] != -1) continue;
-			q.push(val);
-			dist[val] = dist[cur] + 1;
-			minDist = max(minDist, dist[val]);
+			answer.push(cur);
+			continue;
+		}
+
+		for (auto v : towns[cur])
+		{
+			if (visited[v]) continue;
+			q.push({ v, dist + 1 });
+			visited[v] = true;
 		}
 	}
 
-	for (int i = 0; i < dist.size(); i++)
+	if (answer.empty())
 	{
-		if (dist[i] == K) answer.push_back(i);
+		cout << -1;
+		return;
 	}
 
-	if (answer.size())
+	while (!answer.empty())
 	{
-		for (const int& val : answer)
-		{
-			cout << val << "\n";
-		}
+		cout << answer.top() << "\n";
+		answer.pop();
 	}
-	else cout << -1;
+	return;
 }
 
 int main()
