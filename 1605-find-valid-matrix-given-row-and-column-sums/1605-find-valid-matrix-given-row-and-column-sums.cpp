5c621c8dd2c250fbe1f6010fe10d2783b52c4dc8
선우 <51250442+InSange@users.noreply.github.com>
2024-07-21 13:13:22 +0900

    Time: 37 ms (60.05%), Space: 36 MB (16.93%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,45 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        vector<vector<int>> ans;
-        int rsize = rowSum.size();
-        int csize = colSum.size();
-        int rindex = 0;
-        int cindex = 0;
-        
-        ans.assign(rsize, vector<int>(csize, 0));
-        
-        while(rindex < rsize || cindex < csize)
-        {
-            if(rindex >= rsize)
-            {
-                ans[rindex-1][cindex] = colSum[cindex];
-                cindex++;
-                continue;
+        int numRows = rowSum.size();
+        int numCols = colSum.size();
+        vector<vector<int>> result(numRows, vector<int>(numCols, 0));
+
+        int i = 0, j = 0;
+
+        while (i < numRows && j < numCols) {
+            int val = min(rowSum[i], colSum[j]);
+            result[i][j] = val;
+            rowSum[i] -= val;
+            colSum[j] -= val;
+
+            if (rowSum[i] == 0) {
+                i++;
             }
-            else if(cindex >= csize)
-            {
-                ans[rindex][cindex-1] = rowSum[rindex];
-                rindex++;
-                continue;
-            }
-            
-            int val = min(colSum[cindex], rowSum[rindex]);
-            
-            ans[rindex][cindex] = val;
-            rowSum[rindex] -= val;
-            colSum[cindex] -= val;
-            
-            if(!rowSum[rindex])
-            {
-                rindex++;
-            }
-            if(!colSum[cindex])
-            {
-                cindex++;
+            if (colSum[j] == 0) {
+                j++;
             }
         }
-        
-        return ans;
+        return result;
     }
 };
