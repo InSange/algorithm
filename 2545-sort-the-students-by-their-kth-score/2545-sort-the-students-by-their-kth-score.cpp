3347b949f4bb6113837bbbaa6322c10049148dd3
선우 <51250442+InSange@users.noreply.github.com>
2024-07-22 23:41:06 +0900

    Time: 49 ms (49.11%), Space: 30.7 MB (35.19%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cpp
@@ -5,8 +5,6 @@ public:
         int rowNum = score.size();
         int colNum = score[0].size();
         
-        ans.assign(rowNum, vector<int>(colNum, 0));
-        
         priority_queue<pair<int, int>> pq;
         
         for(int i = 0; i < rowNum; i++)
@@ -14,18 +12,12 @@ public:
             pq.push({score[i][k], i});
         }
         
-        int rowIndex = 0;
         while(!pq.empty())
         {
             int curRow = pq.top().second;
             pq.pop();
             
-            for(int i = 0; i < colNum; i++)
-            {
-                ans[rowIndex][i] = score[curRow][i];
-            }
-            
-            rowIndex++;
+            ans.push_back(score[curRow]);
         }
         
         return ans;
