1fac4a6cb7fb67bf1343d04b6f83111bd04e162c
선우 <51250442+InSange@users.noreply.github.com>
2024-08-17 19:15:11 +0900

    Time: 13 ms (6.23%), Space: 17.7 MB (9.86%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        
+        for(string detail : details)
+        {
+            string age = "";
+            age += detail[11];
+            age += detail[12];
+            
+            if(stoi(age) > 60) ans++; 
+        }
+        
+        return ans;
+    }
+};
