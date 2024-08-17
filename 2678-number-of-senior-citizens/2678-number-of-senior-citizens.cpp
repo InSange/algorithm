c1a0f79b6c98a6bb7894c650999d522cbf7eddfb
선우 <51250442+InSange@users.noreply.github.com>
2024-08-17 19:23:56 +0900

    Time: 15 ms (6.23%), Space: 17.7 MB (48.14%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.cpp
@@ -2,14 +2,26 @@ class Solution {
 public:
     int countSeniors(vector<string>& details) {
         int ans = 0;
+        sort(details.begin(), details.end(), [](const string& a, const string& b)
+             {
+                 if(a[11] == b[11]) return a[12] > b[12];
+                 return a[11] > b[11];
+             });
         
         for(string detail : details)
         {
-            string age = "";
-            age += detail[11];
-            age += detail[12];
-            
-            if(stoi(age) > 60) ans++; 
+            cout << detail << "\n";
+            if(detail[11] > '6')
+            {
+                ans++;
+                continue;
+            }
+            if(detail[11] == '6' && detail[12] > '0')
+            {
+                ans++;
+                continue;
+            }
+            else break;
         }
         
         return ans;
