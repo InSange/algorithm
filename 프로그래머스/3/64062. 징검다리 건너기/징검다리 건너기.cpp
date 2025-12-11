980b6bd68b8dd4202c23e9293f816d03ea556b0b
선우 <51250442+InSange@users.noreply.github.com>
2025-12-11 20:11:41 +0900

    [level 3] Title: 징검다리 건너기, Time: 24.05 ms, Memory: 13.6 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -1,120 +1,74 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
-using pii = pair<int, int>;
 
-// 이진 탐색
-int solution(vector<int> stones, int k) {
-    int answer = 0;
-    
-    int left = 1;
-    int right = 200000000;
-    
-    while(left <= right)
-    {
-        int mid = (left + right) / 2;
-        
-        int skip = 0;
-        
-        for(int stone : stones)
-        {
-            if(stone < mid)
-            {
-                skip++;
-                if(skip >= k) break;
-            }
-            else
-            {
-                skip = 0;
-            }
-        }
-        
-        if(skip >= k)
-        {
-            right = mid - 1;
-        }
-        else
-        {
-            answer = mid;
-            left = mid + 1;
-        }
-    }
-    
-    return answer;
+struct Node {
+    int value;
+    int index;
+};
+
+// 부모 노드를 찾는 함수 (경로 압축 적용)
+int findParent(vector<int>& parent, int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findParent(parent, parent[x]);
 }
 
-// 방법 2, 현재 징검다리가 0이 되면 다음 징검다리 돌한테 너 한칸 뛰어야해! 라고 알려주고(요구 점프 값 증가), 그 값이 k가 되면 끝. 효율성 13번에서 걸림.
-/*
-int solution(vector<int> stones, int k) {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    int ssize = stones.size();
-    
-    vector<int> needJump; // 점프 요구치
-    needJump.assign(ssize+1, 0);
-    
-    for(int i = 0; i < ssize; i++)
-    {
-        pq.push({stones[i], i + 1});
-    }
-    
-    int niniz = 0;
-    
-    while(1)
-    {
-        pii curStones = pq.top();
-        pq.pop();
-        
-        while(curStones.first > niniz) niniz++; // 가장 적은 수의 징검다리를 건널 수 있는 니니즈의 수
-        
-        needJump[curStones.second] = needJump[curStones.second - 1] + 1;
+// 두 집합을 합치는 함수 (구멍 크기 갱신 포함)
+void unionSets(vector<int>& parent, vector<int>& size, int a, int b) {
+    int rootA = findParent(parent, a);
+    int rootB = findParent(parent, b);
 
-        int cntK = needJump[curStones.second];
-        while(curStones.second <= ssize && needJump[curStones.second + 1] > 0)
-        {
-            needJump[curStones.second+1] = needJump[curStones.second] + 1;
-            cntK = needJump[curStones.second+1];
-            curStones.second++;
-        }
-        
-        if(cntK >= k)break;
+    if (rootA != rootB) {
+        // b를 a에 붙임 (순서는 상관없으나 크기 관리를 위해 통일)
+        parent[rootB] = rootA;
+        size[rootA] += size[rootB]; // a의 그룹 크기에 b의 그룹 크기를 더함
     }
-    
-    return niniz;
 }
-*/
-// 방법 1 , 0이되면 양쪽을 탐색해서 k를 넘어가는지 확인.
-/*
+
+bool compareNodes(const Node& a, const Node& b) {
+    return a.value < b.value;
+}
+
 int solution(vector<int> stones, int k) {
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    int ssize = stones.size();
+    int n = stones.size();
     
-    for(int i = 0; i < ssize; i++)
-    {
-        pq.push({stones[i], i});
-        //cout << pq.top().first << ", " << pq.top().second << "\n";
+    // Union-Find용 배열 초기화
+    vector<int> parent(n);
+    vector<int> size(n, 1); // 각 돌은 처음에 크기 1짜리 구멍
+    vector<bool> visited(n, false); // 해당 돌이 0이 되었는지 체크
+    vector<Node> nodes(n);
+
+    // 1. 돌들을 값 기준으로 정렬하기 위해 저장
+    for (int i = 0; i < n; i++) {
+        parent[i] = i; // 자기 자신이 부모
+        nodes[i] = {stones[i], i};
     }
-    
-    int niniz = 0;
-    
-    while(1)
-    {
-        pii curStones = pq.top();
-        pq.pop();
-        
-        while(curStones.first > niniz) niniz++; // 가장 적은 수의 징검다리를 건널 수 있는 니니즈의 수
-        stones[curStones.second] -= niniz; // 해당 징검다리를 0으로 만듬.
-        
-        int check = 1;
-        int left = curStones.second, right = curStones.second;
-        while(left >= 0 && stones[--left] == 0) check++;
-        while(right < ssize && stones[++right] == 0) check++;
-        
-        if(check >= k) break;
+    sort(nodes.begin(), nodes.end(), compareNodes);
+
+    // 2. 값이 작은 돌부터 차례대로 제거 (0으로 만들기)
+    for (int i = 0; i < n; i++) {
+        int idx = nodes[i].index;
+        int val = nodes[i].value;
+
+        visited[idx] = true; // 현재 돌 제거 처리
+
+        // 왼쪽 돌이 이미 제거된 상태라면 합침
+        if (idx - 1 >= 0 && visited[idx - 1]) {
+            unionSets(parent, size, idx - 1, idx);
+        }
+        // 오른쪽 돌이 이미 제거된 상태라면 합침
+        if (idx + 1 < n && visited[idx + 1]) {
+            unionSets(parent, size, idx, idx + 1);
+        }
+
+        // 합친 후, 현재 그룹(구멍)의 크기가 k 이상인지 확인
+        int root = findParent(parent, idx);
+        if (size[root] >= k) {
+            return val;
+        }
     }
-    
-    return niniz;
+    return 0;
 }
-*/
