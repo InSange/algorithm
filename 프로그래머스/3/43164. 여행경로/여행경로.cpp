3f39ddacb49398da19ae7bdbb07117e9d74daf12
선우 <51250442+InSange@users.noreply.github.com>
2025-12-14 20:17:47 +0900

    [level 3] Title: 여행경로, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -5,45 +5,35 @@
 
 using namespace std;
 
-vector<string> answer;
-int allCnt = 0;
-vector<bool> visited;
-bool f = false;
-
-void dfs(string cur, vector<vector<string>>& tickets, int cnt)
+bool dfs(string cur, vector<vector<string>>& tickets, vector<bool>& visited, vector<string>& route, int cnt)
 {
-    if(cnt == allCnt) 
-    {
-        f = true;
-        return ;
-    }
+    route.push_back(cur);
     
-    for(int i = 0; i < allCnt; i++)
+    if(cnt == visited.size()) return true;
+    
+    for(int i = 0; i < tickets.size(); i++)
     {
         if(visited[i]) continue;
         if(cur != tickets[i][0]) continue;
         
         visited[i] = true;
-        dfs(tickets[i][1], tickets, cnt + 1);
-        if(f)
-        {
-            answer.push_back(tickets[i][1]);
-            return;
-        }
+        if(dfs(tickets[i][1], tickets, visited, route, cnt + 1)) return true;
+        
+        route.pop_back();
         visited[i] = false;
     }
+    
+    return false;
 }
 
 // 항공권을 모두 사용 = 모든 도시를 방문했다!
 vector<string> solution(vector<vector<string>> tickets) {
-    allCnt = tickets.size();
-    visited.assign(allCnt, false);
+    vector<string> answer;
+    vector<bool> visited(tickets.size(), false);
     
     sort(tickets.begin(), tickets.end()); // 알파벳 순서 정렬
     
-    dfs("ICN", tickets, 0);
-    answer.push_back("ICN");
-    reverse(answer.begin(), answer.end());
+    dfs("ICN", tickets, visited, answer, 0);
     
     return answer;
 }
