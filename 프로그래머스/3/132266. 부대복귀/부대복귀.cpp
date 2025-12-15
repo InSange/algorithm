c7e8d6b4d26589554431de6452c85df81cf0ae5d
선우 <51250442+InSange@users.noreply.github.com>
2025-12-15 15:26:06 +0900

    [level 3] Title: 부대복귀, Time: 97.49 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.cpp
@@ -8,35 +8,35 @@ using namespace std;
 // des에서 dfs로 거리 업데이트
 vector<int> solution(int n, vector<vector<int>> roads, vector<int> sources, int destination) {
     vector<int> answer;
-    vector<int> visited(n + 1, -1);
-    vector<vector<int>> dist(n + 1, vector<int>());
+    vector<int> distances(n + 1, -1);
+    vector<vector<int>> graph(n + 1, vector<int>());
     
-    for(vector<int> road : roads)
+    for(const vector<int>& road : roads)
     {
-        dist[road[0]].push_back(road[1]);
-        dist[road[1]].push_back(road[0]);
+        graph[road[0]].push_back(road[1]);
+        graph[road[1]].push_back(road[0]);
     }
     
     queue<int> q;
     q.push(destination);
-    visited[destination] = 0;
+    distances[destination] = 0;
     
     while(!q.empty())
     {
         int cur = q.front();
         q.pop();
         
-        for(int next : dist[cur])
+        for(int next : graph[cur])
         {
-            if(visited[next] != -1) continue;
-            visited[next] = visited[cur] + 1;
+            if(distances[next] != -1) continue;
+            distances[next] = distances[cur] + 1;
             q.push(next);
         }
     }
     
     for(int s : sources)
     {
-        answer.push_back(visited[s]);
+        answer.push_back(distances[s]);
     }
     
     return answer;
