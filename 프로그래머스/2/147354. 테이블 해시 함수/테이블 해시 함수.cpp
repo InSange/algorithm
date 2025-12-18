64b791797eacd16731749a4cfcf9c154c44cb9ce
선우 <51250442+InSange@users.noreply.github.com>
2025-12-19 03:10:14 +0900

    [level 2] Title: 테이블 해시 함수, Time: 4.77 ms, Memory: 57.3 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -5,34 +5,25 @@
 
 using namespace std;
 
-int index = 0;
-
-bool compare(const vector<int>& a, const vector<int>& b)
-{
-    if(a[index] == b[index]) return a[0] > b[0];
-    else return a[index] < b[index];
-}
-
 int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int answer = 0;
-    vector<int> S_i;
-    
-    index = col-1;
-    sort(data.begin(), data.end(), compare);
+
+    sort(data.begin(), data.end(),[col] (const vector<int>& a, const vector<int>& b) {
+        int idx = col - 1;
+        if(a[idx] == b[idx]) return a[0] > b[0];
+        return a[idx] < b[idx];
+    });
     
     for(int i = row_begin; i <= row_end; i++)
     {
         int sum = 0;
-        for(int& v : data[i-1])
+        int row_idx = i - 1;
+        
+        for(int& v : data[row_idx])
         {
             sum += (v%i);
         }
-        S_i.push_back(sum);
-    }
-    
-    for(int& i : S_i)
-    {
-        answer ^= i;
+        answer ^= sum;
     }
     
     return answer;
