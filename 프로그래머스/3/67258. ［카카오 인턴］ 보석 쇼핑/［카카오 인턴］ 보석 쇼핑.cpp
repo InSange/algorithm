117b583ec712d049dbf4ede2bc7fa6981095c83e
선우 <51250442+InSange@users.noreply.github.com>
2025-12-22 16:10:21 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 34.11 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -7,39 +7,41 @@
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    vector<int> answer(2, -1);
-    set<string> gemSet;
-    unordered_map<string, int> gemCnt;
+    vector<int> answer = {0, 0};
+    unordered_map<string, int> m;
+    set<string> s1;
+    int start = 0, end = 0, gsize = gems.size(), ssize = 0, len = 100001;
     
-    for(const string& gem : gems) gemSet.insert(gem);
-    for(const string& gem : gemSet) gemCnt[gem] = 0;
-    cout << gemSet.size() << gemCnt.size() << "\n";
-    
-    int start = 0;
-    set<string> curSet;
-    
-    for(int end = 0; end < gems.size(); end++)
-    {      
-        // 보석 추가
-        curSet.insert(gems[end]);
-        gemCnt[gems[end]]++;
-        
-        if(curSet.size() != gemSet.size()) continue;
-        // start의 보석이 2개 이상일 경우 start를 업데이트하기
-        while(gemCnt[gems[start]] > 1) 
-        {
-            gemCnt[gems[start]]--;
-            start++;
-        }
-        
-        // answer 값이 업데이트 안되었을 때(-1) or 이전의 answer 구간 길이보다 현재가 짧을 때
-        if(answer[0] == -1 || (answer[1] - answer[0]) > (end - start))
-        {
-            answer[0] = start + 1;
-            answer[1] = end + 1;
-        }
+    for(int i = 0; i < gsize; i++)
+    {
+        s1.insert(gems[i]);
     }
     
+    while(start <= end && start < gsize)
+    {
+        if(ssize == s1.size() && (end-start)+1 < len)
+        {
+            len = end-start+1;
+            answer[0] = start+1;
+            answer[1] = end;
+        }
+
+        if(ssize < s1.size() && end < gsize)
+        {
+            m[gems[end]]++;
+            if(m[gems[end]] == 1) ssize++;
+            end++;
+        }
+        else 
+        {
+            m[gems[start]]--;
+            if(m[gems[start]] == 0)
+            {
+                ssize--;
+            }
+            start++;
+        }
+    }
     
     return answer;
 }
