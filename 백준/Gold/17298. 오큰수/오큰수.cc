3cb2dad7375decf3da04dac9a4784997527ac154
선우 <51250442+InSange@users.noreply.github.com>
2025-12-23 06:53:52 +0900

    [Gold IV] Title: 오큰수, Time: 184 ms, Memory: 12504 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -5,50 +5,72 @@
 
 using namespace std;
 
+//void Solve()
+//{
+//	vector<int> v;
+//	int N, num;
+//
+//	cin >> N;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> num;
+//		v.push_back(num);
+//	}
+//
+//	vector<int> answer;
+//	stack<int> st;
+//	int index = N - 1;
+//	st.push(v[index]);
+//	answer.push_back(-1);
+//
+//	index--;
+//
+//	for (; index >= 0; index--)
+//	{
+//		if (st.empty()) answer.push_back(-1);
+//		else
+//		{
+//			while (!st.empty())
+//			{
+//				if (st.top() <= v[index]) st.pop();
+//				else
+//				{
+//					answer.push_back(st.top());
+//					break;
+//				}
+//			}
+//
+//			if(st.empty()) answer.push_back(-1);
+//		}
+//		st.push(v[index]);
+//	}
+//
+//	reverse(answer.begin(), answer.end());
+//
+//	for (const int& n : answer) cout << n << " ";
+//}
+
 void Solve()
 {
-	vector<int> v;
-	int N, num;
-
+	int N;
 	cin >> N;
+	vector<int> v(N);
 
-	for (int i = 0; i < N; i++)
-	{
-		cin >> num;
-		v.push_back(num);
-	}
+	for (int i = 0; i < N; i++) cin >> v[i];
 
-	vector<int> answer;
+	vector<int> answer(N, -1);
 	stack<int> st;
-	int index = N - 1;
-	st.push(v[index]);
-	answer.push_back(-1);
 
-	index--;
-
-	for (; index >= 0; index--)
+	for (int i = N - 1; i >= 0; i--)
 	{
-		if (st.empty()) answer.push_back(-1);
-		else
-		{
-			while (!st.empty())
-			{
-				if (st.top() <= v[index]) st.pop();
-				else
-				{
-					answer.push_back(st.top());
-					break;
-				}
-			}
+		while (!st.empty() && st.top() <= v[i]) st.pop();
 
-			if(st.empty()) answer.push_back(-1);
-		}
-		st.push(v[index]);
+		if (!st.empty()) answer[i] = st.top();
+		st.push(v[i]);
 	}
 
-	reverse(answer.begin(), answer.end());
-
-	for (const int& n : answer) cout << n << " ";
+	for(const int& n : answer	) cout << n << " ";
 }
 
 int main()
