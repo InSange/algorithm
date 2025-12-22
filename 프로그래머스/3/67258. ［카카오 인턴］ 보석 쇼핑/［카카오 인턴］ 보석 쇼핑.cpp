e2919ac483a097a6280844fb8fccfa9d76ecbcbc
선우 <51250442+InSange@users.noreply.github.com>
2025-12-22 16:26:50 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 30.76 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -8,38 +8,30 @@ using namespace std;
 
 vector<int> solution(vector<string> gems) {
     vector<int> answer = {0, 0};
+    set<string> gemSet(gems.begin(), gems.end());
+    int setCnt = gemSet.size();
+    
     unordered_map<string, int> m;
-    set<string> s1;
-    int start = 0, end = 0, gsize = gems.size(), ssize = 0, len = 100001;
+    int start = 0, minLen = 100001;
     
-    for(int i = 0; i < gsize; i++)
+    for(int end = 0; end < gems.size(); end++)
     {
-        s1.insert(gems[i]);
-    }
-    
-    while(start <= end && start < gsize)
-    {
-        if(ssize == s1.size() && (end-start)+1 < len)
+        m[gems[end]]++;
+        
+        if(m.size() == setCnt)
         {
-            len = end-start+1;
-            answer[0] = start+1;
-            answer[1] = end;
-        }
-
-        if(ssize < s1.size() && end < gsize)
-        {
-            m[gems[end]]++;
-            if(m[gems[end]] == 1) ssize++;
-            end++;
-        }
-        else 
-        {
-            m[gems[start]]--;
-            if(m[gems[start]] == 0)
+            while(m[gems[start]] > 1)
             {
-                ssize--;
+                m[gems[start]]--;
+                start++;
             }
-            start++;
+            
+            if(end - start < minLen)
+            {
+                minLen = end-start;
+                answer[0] = start+1;
+                answer[1] = end+1;
+            }
         }
     }
     
