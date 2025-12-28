d6e24a3599d44c8e8a2dbc23179349a1a488e67f
선우 <51250442+InSange@users.noreply.github.com>
2025-12-28 22:21:59 +0900

    [level 2] Title: 요격 시스템, Time: 183.78 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.cpp
@@ -4,29 +4,26 @@
 #include <iostream>
 
 using namespace std;
-using pii = pair<int,int>;
+
+bool cmp(const vector<int>& a, const vector<int>& b)
+{
+    return a[1] < b[1];
+}
 
 int solution(vector<vector<int>> targets) {
-    int answer = 1;
-    sort(targets.begin(), targets.end());
-    int se = targets[0][0], ee = targets[0][1]; // se 시작은 가장 짧은 s를 찾고 끝은 e부분이 가장 작은 부분을 찾아서 asnwer 값을 갱신하기.
+    int answer = 0;
     
-    for(int i = 1; i < targets.size(); i++)
-    {
-        if(ee <= targets[i][0]) //그다음 타겟의 s가 현재의 e보다 크거나 같을 경우
+    sort(targets.begin(), targets.end(), cmp);
+    
+    int lastEnd = -1;
+    
+    for(const auto& target : targets) {
+        int s = target[0], e = target[1];
+        
+        if(s >= lastEnd)
         {
             answer++;
-            se = targets[i][0];
-            ee = targets[i][1];
-        }
-        else if(ee <= targets[i][1])// ee 범위가 넘어갈 경우.
-        {
-            se = targets[i][0];
-        }
-        else if(ee > targets[i][1])
-        {
-            se = targets[i][0];
-            ee = targets[i][1];
+            lastEnd = e;
         }
     }
     
