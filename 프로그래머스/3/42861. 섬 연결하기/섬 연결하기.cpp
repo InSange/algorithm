820739f84d0c77340b27f8e375199280c463ea62
선우 <51250442+InSange@users.noreply.github.com>
2026-02-01 00:58:15 +0900

    [level 3] Title: 섬 연결하기, Time: 0.03 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
 
 using namespace std;
 using pii = pair<int, int>;
@@ -9,39 +8,42 @@ using pii = pair<int, int>;
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
     
-    vector<vector<pii>> v(n, vector<pii>());
-    vector<int> dist(n, 1e9);
+    // 1. 인접 리스트 생성
+    vector<vector<pii>> adj(n);
+    for(const auto& cost : costs) {
+        adj[cost[0]].push_back({cost[2], cost[1]});
+        adj[cost[1]].push_back({cost[2], cost[0]});
+    }
+    
+    // 2. Prim 알고리즘 준비
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
     vector<bool> visited(n, false);
     
-    for(const auto& cost : costs)
-    {   // 0번 지점 <- 비용, 1번지점
-        v[cost[0]].push_back({cost[2], cost[1]});
-        v[cost[1]].push_back({cost[2], cost[0]});
-    }
+    // 임의의 시작점 (0번 노드)
+    pq.push({0, 0}); // {비용, 노드}
     
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    
-    pq.push({0, 0});
-    
-    while(!pq.empty())
-    {
-        pii cur = pq.top();
+    while(!pq.empty()) {
+        int cost = pq.top().first;
+        int cur = pq.top().second;
         pq.pop();
         
-        if(visited[cur.second]) continue;
-        visited[cur.second] = true;
-        dist[cur.second] = cur.first;
+        // 이미 연결된 섬이면 패스
+        if(visited[cur]) continue;
         
-        for(const auto& bridge : v[cur.second])
-        {
-            if(dist[bridge.second] <= bridge.first) continue;
-            pq.push(bridge);
+        // [핵심] 연결 확정! 여기서 바로 비용 더하기
+        visited[cur] = true;
+        answer += cost;
+        
+        // 다음 간선 탐색
+        for(const auto& next : adj[cur]) {
+            int next_cost = next.first;
+            int next_node = next.second;
+            
+            // [최적화] 이미 방문한(연결된) 노드로 가는 간선은 굳이 큐에 넣지 않음
+            if(!visited[next_node]) {
+                pq.push({next_cost, next_node});
+            }
         }
-    }
-    
-    for(const int& d : dist)
-    {
-        answer += d;
     }
     
     return answer;
