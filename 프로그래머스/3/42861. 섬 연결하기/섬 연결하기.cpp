120d077b10a83bb2f9dd099b97294f722a150511
선우 <51250442+InSange@users.noreply.github.com>
2026-02-01 01:38:19 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -1,48 +1,42 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
+#include <functional>
 
 using namespace std;
-using pii = pair<int, int>;
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
     
-    // 1. 인접 리스트 생성
-    vector<vector<pii>> adj(n);
-    for(const auto& cost : costs) {
-        adj[cost[0]].push_back({cost[2], cost[1]});
-        adj[cost[1]].push_back({cost[2], cost[0]});
-    }
+    vector<int> parent(n);
+    for(int i = 0; i < n; i++) parent[i] = i;
     
-    // 2. Prim 알고리즘 준비
-    priority_queue<pii, vector<pii>, greater<pii>> pq;
-    vector<bool> visited(n, false);
+    function<int(int)> getParent = [&](int x) -> int {
+        if(parent[x] == x) return x;
+        return parent[x] = getParent(parent[x]);
+    };
     
-    // 임의의 시작점 (0번 노드)
-    pq.push({0, 0}); // {비용, 노드}
+    auto unionFind = [&](int a, int b) -> bool {
+        a = getParent(a);
+        b = getParent(b);
+        
+        if(a == b) return false;
+        
+        if(a < b) parent[a] = b;
+        else parent[b] = a;
+        
+        return true;
+    };
     
-    while(!pq.empty()) {
-        int cost = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-        
-        // 이미 연결된 섬이면 패스
-        if(visited[cur]) continue;
-        
-        // [핵심] 연결 확정! 여기서 바로 비용 더하기
-        visited[cur] = true;
-        answer += cost;
-        
-        // 다음 간선 탐색
-        for(const auto& next : adj[cur]) {
-            int next_cost = next.first;
-            int next_node = next.second;
-            
-            // [최적화] 이미 방문한(연결된) 노드로 가는 간선은 굳이 큐에 넣지 않음
-            if(!visited[next_node]) {
-                pq.push({next_cost, next_node});
-            }
+    sort(costs.begin(), costs.end(), [](const auto& a, const auto& b){
+        return a[2] < b[2];
+    });
+    
+    for(const auto& cost : costs)
+    {
+        if(unionFind(cost[0], cost[1]))
+        {
+            answer += cost[2];
         }
     }
     
