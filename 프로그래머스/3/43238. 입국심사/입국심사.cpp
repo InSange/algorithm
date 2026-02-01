aa5d64d8fabb0ba40d0a0882955f89b94d5a5860
선우 <51250442+InSange@users.noreply.github.com>
2026-02-01 18:13:37 +0900

    [level 3] Title: 입국심사, Time: 48.77 ms, Memory: 6.83 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -7,16 +7,17 @@ using namespace std;
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
+   
     sort(times.begin(), times.end());
     
-    long long left = 1, right = (long long)times[times.size() - 1] * n;
+    long long left = times[0], right = (long long)times[times.size()-1] * n;
     
     while(left <= right)
     {
         long long mid = (left + right)/2;
         long long sum = 0;
         
-        for(const int& time : times)
+        for(const auto& time : times)
         {
             sum += (mid / time);
         }
@@ -27,8 +28,8 @@ long long solution(int n, vector<int> times) {
         }
         else
         {
-            answer = mid;
             right = mid - 1;
+            answer = mid;
         }
     }
     
