e0c1e15d5319e99a6c344421200d6643f08d6367
선우 <51250442+InSange@users.noreply.github.com>
2026-02-06 14:19:02 +0900

    [level 3] Title: 합승 택시 요금, Time: 2.53 ms, Memory: 7.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -0,0 +1,61 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+using pii = pair<int, int>;
+
+void dijkstra(vector<int>& result, const vector<vector<pii>>& graph, int start)
+{
+    auto cmp = [](const pii& a, const pii& b) {
+        return a.second > b.second; // 비용 기준
+    };
+    
+    priority_queue<pii, vector<pii>, decltype(cmp)> pq(cmp);
+    pq.push({start , 0}); // 인덱스, 비용
+    
+    result[start] = 0;
+    
+    while(!pq.empty())
+    {
+        pii cur = pq.top();
+        pq.pop();
+        
+        for(int i = 0; i < graph[cur.first].size(); i++)
+        {
+            pii next = graph[cur.first][i];
+            
+            if(result[next.first] > cur.second + next.second)
+            {
+                result[next.first] = cur.second + next.second;
+                pq.push({next.first, result[next.first]});
+            }
+        }
+    }
+}
+
+int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = 1e9;
+    
+    vector<vector<pii>> graph(n + 1);
+    
+    for(int i = 0; i < fares.size(); i++)
+    {
+        graph[fares[i][0]].push_back({fares[i][1], fares[i][2]}); // 인덱스, 요금
+        graph[fares[i][1]].push_back({fares[i][0], fares[i][2]});
+    }
+    
+    vector<vector<int>> results(3, vector<int>(n+1, 1e9));
+    
+    dijkstra(results[0], graph, s);
+    dijkstra(results[1], graph, a);    
+    dijkstra(results[2], graph, b);
+    
+    for(int i = 1; i <= n; i++)
+    {
+        if(results[0][i] == 1e9 || results[1][i] == 1e9 || results[2][i] == 1e9) continue;
+        answer = min(answer, results[0][i] + results[1][i] + results[2][i]);
+    }
+    
+    return answer;
+}
