2d2800173d3e28bbf9f1b913b09b2ec661304937
선우 <51250442+InSange@users.noreply.github.com>
2026-02-09 12:42:12 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 16.80 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.cpp
@@ -1,38 +1,44 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <set>
+#include <unordered_set>
 #include <iostream>
 
 using namespace std;
 
 vector<int> solution(vector<string> gems) {
-    vector<int> answer = {0, 0};
-    set<string> gemSet(gems.begin(), gems.end());
-    int setCnt = gemSet.size();
+    vector<int> answer;
+    answer.push_back(0), answer.push_back(gems.size());
     
-    unordered_map<string, int> m;
-    int start = 0, minLen = 100001;
+    unordered_set<string> kinds;
+    kinds.insert(gems.begin(), gems.end());
     
-    for(int end = 0; end < gems.size(); end++)
+    unordered_map<string, int> gemCnt;
+    unordered_set<string> curGems;
+    int left = 0, right = 0;
+    
+    while(right < gems.size() && left <= right)
     {
-        m[gems[end]]++;
+        gemCnt[gems[right]]++;
+        if(gemCnt[gems[right]] == 1) curGems.insert(gems[right]);
         
-        if(m.size() == setCnt)
+        while(curGems.size() == kinds.size())
         {
-            while(m[gems[start]] > 1)
+            if((answer[1] - answer[0]) > (right - left))
             {
-                m[gems[start]]--;
-                start++;
+                answer[0] = left+1;
+                answer[1] = right+1;    
             }
-            
-            if(end - start < minLen)
+
+            gemCnt[gems[left]]--;
+            if(gemCnt[gems[left]] == 0) // 음수로 될 일은 없음.
             {
-                minLen = end-start;
-                answer[0] = start+1;
-                answer[1] = end+1;
+                curGems.erase(gems[left]);
             }
+            left++;
         }
+        
+        right++;
     }
     
     return answer;
