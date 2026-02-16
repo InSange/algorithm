b51889907eeffcb90ee18ec8fc7385e2ebb0bc1e
선우 <51250442+InSange@users.noreply.github.com>
2026-02-16 23:10:34 +0900

    Time: 5 ms (57%), Space: 19.1 MB (91.29%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
-bool dfs(int node, vector<int>adj[],vector<int>&vis,vector<int>&path){
-    vis[node]=1;
-    path[node]=1;
-    for(auto it:adj[node]){
-        if(!vis[it]){
-            if(dfs(it,adj,vis,path)==true)return true;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> graph(numCourses);
+        vector<int> indegree(numCourses, 0);
+
+        for(auto& pre : prerequisites)
+        {
+            int u = pre[1];
+            int v = pre[0];
+
+            graph[u].push_back(v);
+            indegree[v]++;
         }
-        else if(path[it])return true;
-    }
-    path[node]=0;
-    return false;
-}
-    bool canFinish(int n, vector<vector<int>>& pre) {
-        vector<int>vis(n,0);
-        vector<int>path(n,0);
-        vector<int>adj[n];
-        for(int i =0;i<pre.size();i++){
-            adj[pre[i][1]].push_back(pre[i][0]);
+
+        queue<int> q;
+
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(indegree[i] == 0) q.push(i);
         }
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,path)==true)return false;
+
+        int cnt = 0;
+        while(!q.empty())
+        {
+            int cur = q.front();
+            q.pop();
+
+            cnt++;
+
+            for(int next : graph[cur])
+            {
+                indegree[next]--;
+                if(indegree[next] == 0) q.push(next);
             }
         }
-        return true;
+        return cnt == numCourses;
     }
 };
