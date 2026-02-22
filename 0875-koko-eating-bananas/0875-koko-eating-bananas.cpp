b62f4a2cfe0bb10d759fb1c43d9148e34ec5f722
선우 <51250442+InSange@users.noreply.github.com>
2026-02-22 18:38:46 +0900

    Time: 31 ms (5.26%), Space: 22.9 MB (77.62%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        // piles 크기보다 h가 같거나 더 큼. 가장 큰 값을 처리하면 h시간만큼 걸림
+        int left = 1, right = 0;
+
+        for(int & pile : piles)
+        {
+            right = max(right, pile);
+        }
+
+        int k = 0;
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+
+            unsigned int time = 0;
+            for(int& pile : piles)
+            {
+                time += pile/mid;
+                if(pile%mid != 0) time++;
+            }
+
+            if(time > h)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                k = mid;
+                right = mid-1;
+            }
+            cout << left << " , "<< right << ", " << k <<"\n";
+        }
+
+        return k;
+    }
+};
