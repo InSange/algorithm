4b9eba265cda1a469786d10114a95181f1a50ae1
선우 <51250442+InSange@users.noreply.github.com>
2026-02-25 03:29:59 +0900

    Time: 104 ms (5.24%), Space: 25.4 MB (5.96%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1) return intervals;
+
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            if(a[0] == b[0]) return a[1] < b[1];
+            return a[0] < b[0];
+        });
+
+        for(auto interval : intervals)
+        {
+            cout << interval[0] << " , " << interval[1] << "\n";
+        }
+
+        vector<vector<int>> answer;
+        int left = 0, right = 1;
+        while(right <= intervals.size())
+        {
+            vector<int> cur(intervals[left]);
+
+            while((right < intervals.size()) && (cur[1] >= intervals[right][0]))
+            {
+                if(intervals[right][1] > cur[1]) cur[1] = intervals[right][1];
+                right++;
+            }
+
+            answer.push_back(cur);
+            left = right;
+            right++;
+        }
+
+        return answer;
+    }
+};
