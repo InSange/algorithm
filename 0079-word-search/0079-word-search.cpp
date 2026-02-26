3eb18fd2f0d48f615520d65122f880d6b8b0c720
선우 <51250442+InSange@users.noreply.github.com>
2026-02-26 19:05:05 +0900

    Time: 200 ms (71.23%), Space: 10.3 MB (93.54%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    vector<vector<bool>> visited;
     int dx[4] = {0, 0, 1, -1};
     int dy[4] = {1, -1, 0, 0};
 
@@ -9,7 +8,7 @@ public:
         return y < 0 || x < 0 || y >= row || x >= col;
     }
 
-    bool dfs(const vector<vector<char>>& board, string word, int curY, int curX, int index)
+    bool dfs(vector<vector<char>>& board, string& word, int curY, int curX, int index)
     {
         if(index == word.length()) return true;
 
@@ -18,27 +17,24 @@ public:
             int nextY = curY + dy[i], nextX = curX + dx[i];
             if(OutBounds(nextY, nextX, board.size(), board[0].size())) continue;
             if(board[nextY][nextX] != word[index]) continue;
-            if(visited[nextY][nextX]) continue;
 
-            visited[nextY][nextX] = true;
+            board[nextY][nextX] = '#';
             if(dfs(board, word, nextY, nextX, index + 1)) return true;
-            visited[nextY][nextX] = false;
+            board[nextY][nextX] = word[index];
         }
 
         return false;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
-       visited.assign(board.size(), vector<bool>(board[0].size(), false));
-
        for(int i = 0; i < board.size(); i++)
         {
             for(int j = 0; j < board[i].size(); j++)
             {
                 if(board[i][j] != word[0]) continue;
-                visited[i][j] = true;
+                board[i][j] = '#';
                 if(dfs(board, word, i, j, 1)) return true;
-                visited[i][j] = false;
+                board[i][j] = word[0];
             }
         }
 
