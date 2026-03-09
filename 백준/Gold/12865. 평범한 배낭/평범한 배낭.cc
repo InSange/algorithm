9e219a2efb56d45fb49a2e9748b73f1efbf77e28
선우 <51250442+InSange@users.noreply.github.com>
2026-03-10 00:16:06 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,60 +1,45 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int bag[101][100001] = {};
+int N, K, W, V;
+vector<int> dp;
+vector<vector<int>> v;
 
-bool cmp(pair<int, int> p1, pair<int, int> p2)
+void Input()
 {
-	return p1.first < p2.first;
+	cin >> N >> K;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> W >> V;
+
+		v.push_back({ W, V });
+	}
+
+	dp.assign(K + 1, 0);
 }
 
-int MAX(int n1, int n2)
+void Solve()
 {
-	if (n1 > n2) return n1;
-	else return n2;
+	for (int i = 0; i < N; i++)
+	{
+		W = v[i][0], V = v[i][1];
+
+		for (int j = K; j >= W; j--)
+		{
+			dp[j] = max(dp[j], dp[j - W] + V);
+		}
+	}
+
+	cout << dp[K] << "\n";
 }
 
 int main()
 {
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	int N, K;
-	vector<pair<int, int>> v;
-
-	cin >> N >> K;
-	v.push_back(make_pair(0, 0));
-
-	for (int i = 0; i < N; i++) {
-		int W, V;
-		cin >> W >> V;
-
-		v.push_back(make_pair(W, V));
-	}
-
-	sort(v.begin(), v.end(), cmp);
-
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= K; j++)
-		{
-			if (v[i].first > j)
-			{
-				bag[i][j] = bag[i - 1][j];
-			}
-			else
-			{
-				bag[i][j] = MAX(bag[i-1][j - v[i].first] + v[i].second, bag[i-1][j]);
-			}
-			//cout << bag[i][j] << " ";
-		}
-		//cout << "\n";
-	}
-
-	cout << bag[N][K] << "\n";
+	Input();
+	Solve();
 
 	return 0;
 }
